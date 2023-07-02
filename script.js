e4919943d99254689d234b0dfe208732d0fb670a
Merih <mariotezare@gmail.com>
2023-07-02 09:30:19 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript code
+document.addEventListener('DOMContentLoaded', () => {
+    const h1Element = document.querySelector('h1');
+    h1Element.addEventListener('click', () => {
+        alert('You clicked the heading!');
+    });
+});
